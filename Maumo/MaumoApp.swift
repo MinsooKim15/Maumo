8e0bf911a689313a1122679a04e09fb5cf0fad4d
minsookim <minsookim0615@gmail.com>
2021-01-31 22:48:15 +0900

    Initial Commit

---
+++ Maumo/MaumoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaumoApp.swift
+//  Maumo
+//
+//  Created by minsoo kim on 2021/01/31.
+//
+
+import SwiftUI
+
+@main
+struct MaumoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
